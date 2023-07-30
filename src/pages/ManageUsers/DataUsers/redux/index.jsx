192a56a9690e8fe0cfbfa06e16bcbbbc1cbc3761
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-07-30 11:44:01 +0700

    feat: add user add and update

--- src/pages/ManageUsers/DataUsers/redux/index.jsx
+++ src/pages/ManageUsers/DataUsers/redux/index.jsx
@@ -1,9 +1,104 @@
+import { Swal, deleteData, ToastNotification, masterActions, reset, utilityActions, putDataParams, postData } from "../../../../components"
+
 export const simpanDataUsers = () => {
     return async  (dispatch, getState) => {
         const state = getState();
-        const selected = state.form.FormDataUsers?.values;
-        // console.log(state.utility.isEdit)
-        // console.log(selected)
+        const data = state.form.FormDataUsers?.values;
+        const isEdit = state.utility.isEdit;
+
+        if(data.password?.[1]){
+            if (data.password_confirmation?.[1] === undefined) {
+                ToastNotification("info", "Konfirmasi Password tidak boleh kosong")
+                return false
+            }
+        }
+
+        if (data.password?.[1] !== data.password_confirmation?.[1]) {
+            ToastNotification("info", "Password tidak sama")
+            return false
+        }
+
+        dispatch(utilityActions.setLoading(true));
+
+        isEdit
+            ? putDataParams("user/" + data.id, data)
+                .then((res) => {
+                    dispatch(utilityActions.setLoading(false));
+                    if (res.data?.name?.[0]) {
+                        ToastNotification("info", res.data?.name?.[0]);
+                        return false;
+                    }
+                    if (res.data?.email?.[0]) {
+                        ToastNotification("info", res.data?.email?.[0]);
+                        return false;
+                    }
+                    dispatch(masterActions.getDataUsers());
+                    dispatch(utilityActions.hideModal());
+                    dispatch(reset("FormDataUsers"));
+                    ToastNotification("success", "Berhasil mengedit data user");
+                })
+                .catch((err) => {
+                    ToastNotification("info", "Edit data user gagal, silahkan coba lagi !!!");
+                    dispatch(utilityActions.setLoading(false));
+                })
+                : postData("user", data)
+                    .then((res) => {
+                        dispatch(utilityActions.setLoading(false));
+                        if (res.data?.name?.[0]) {
+                            ToastNotification("info", res.data?.name?.[0]);
+                            return false;
+                        }
+                        if (res.data?.email?.[0]) {
+                            ToastNotification("info", res.data?.email?.[0]);
+                            return false;
+                        }
+                        if (res.data?.password?.[0]) {
+                            ToastNotification("info", res.data?.password?.[0]);
+                            return false;
+                        }
+                        dispatch(masterActions.getDataUsers());
+                        dispatch(utilityActions.hideModal());
+                        dispatch(reset("FormDataUsers"));
+                        ToastNotification("success", "Berhasil menambahkan data user");
+                    })
+                    .catch((err) => {
+                        ToastNotification("info", "Tambah data user gagal, silahkan coba lagi !!!");
+                        dispatch(utilityActions.setLoading(false));
+                    })
     }
 }
 
+export const hapusDataUsers = (row) => {
+    return async  (dispatch, getState) => {
+        Swal.fire({
+            // title: "Anda Yakin !!",
+            // text: "Ingin Menghapus Data Ini ?",
+            html:
+              "Apakah Anda Yakin Ingin " +
+              "Menghapus " +
+              "<h1><b>User  " +
+              row.name +
+              "</b> ?</h1>",
+            icon: "warning",
+            position: "top-center",
+            cancelButtonText: "Tidak",
+            showCancelButton: true,
+            confirmButtonText: "OK",
+            showConfirmButton: true,
+          }).then((result) => {
+            if (result.isConfirmed) {
+              deleteData("user/" + row.id)
+                .then((res) => {
+                  ToastNotification("success", "Data berhasil dihapus");
+                  dispatch(masterActions.getDataUsers());
+                })
+                .catch((err) => {
+                  ToastNotification(
+                    "info",
+                    "Terjadi kesalahaan saat menghapus data !!!"
+                  );
+                });
+            }
+          });
+    }
+}
--- src/pages/ManageUsers/DataUsers/tabel/index.jsx
+++ src/pages/ManageUsers/DataUsers/tabel/index.jsx
@@ -8,6 +8,7 @@ import {
   useEffect,
   utilityActions,
 } from "../../../../components";
+import { hapusDataUsers } from "../redux"
 
 const TabelUsers = () => {
   const dispatch = useDispatch();
@@ -28,11 +29,6 @@ const TabelUsers = () => {
       sort: true,
     },
     {
-      dataField: "level",
-      text: "Level",
-    },
-
-    {
       dataField: "action",
       text: "Action",
       csvExport: false,
@@ -41,10 +37,10 @@ const TabelUsers = () => {
         return (
           <div className="row text-center">
             <div className="col-12">
-              <button className="btn btn-primary">
+              <button className="btn btn-primary" onClick={() => showModalUser(row, true)}>
                 <i className="fa fa-edit"></i>
               </button>
-              <button className="btn btn-danger ml-2">
+              <button className="btn btn-danger ml-2" onClick={() => dispatch(hapusDataUsers(row))}>
                 <i className="fa fa-trash"></i>
               </button>
             </div>
@@ -53,10 +49,10 @@ const TabelUsers = () => {
       },
     },
   ];
-  const showModalUser = () => {
-    dispatch(utilityActions.getDataEdit(false));
+  const showModalUser = (data, isEdit) => {
+    dispatch(utilityActions.getDataEdit(data));
     dispatch(utilityActions.showModal());
-    dispatch(utilityActions.isEdit(false));
+    dispatch(utilityActions.isEdit(isEdit));
   };
   return (
     <Tabel
