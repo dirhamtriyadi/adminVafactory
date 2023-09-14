cec984f8077b3793efeb2795d939a2c95b40fb59
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-09-14 13:01:14 +0700

    feat: add color colum UANGMASUK and UANGKELUAR

--- src/pages/UangKas/tabel/index.jsx
+++ src/pages/UangKas/tabel/index.jsx
@@ -23,25 +23,49 @@ const TabelUangkas = () => {
     {
       dataField: "user.name",
       text: "User Id",
+      sort: true,
+      style: function callback(cell, row, rowIndex, colIndex) {
+        // console.log(cell, row, rowIndex, colIndex);
+        return row.cash_flow_type === "UANGMASUK" ? { backgroundColor: "#00FF00" } : { backgroundColor: "#FF0000" };
+      }
     },
     {
       dataField: "transaction_date",
       text: "Tanggal",
-      sort: true
+      sort: true,
+      style: function callback(cell, row, rowIndex, colIndex) {
+        // console.log(cell, row, rowIndex, colIndex);
+        return row.cash_flow_type === "UANGMASUK" ? { backgroundColor: "#00FF00" } : { backgroundColor: "#FF0000" };
+      }
     },
     
     {
       dataField: "description",
       text: "Deskripsi",
+      sort: true,
+      style: function callback(cell, row, rowIndex, colIndex) {
+        // console.log(cell, row, rowIndex, colIndex);
+        return row.cash_flow_type === "UANGMASUK" ? { backgroundColor: "#00FF00" } : { backgroundColor: "#FF0000" };
+      }
     },
     {
       dataField: "cash_flow_type",
       text: "Type",
+      sort: true,
+      style: function callback(cell, row, rowIndex, colIndex) {
+        // console.log(cell, row, rowIndex, colIndex);
+        return row.cash_flow_type === "UANGMASUK" ? { backgroundColor: "#00FF00" } : { backgroundColor: "#FF0000" };
+      }
     },
     {
       dataField: "amount",
       text: "Nominal",
       headerClasses : "text-right",
+      sort: true,
+      style: function callback(cell, row, rowIndex, colIndex) {
+        // console.log(cell, row, rowIndex, colIndex);
+        return row.cash_flow_type === "UANGMASUK" ? { backgroundColor: "#00FF00" } : { backgroundColor: "#FF0000" };
+      },
       formatter: (cell) => {
         return (
           <div className="text-right">
@@ -57,6 +81,7 @@ const TabelUangkas = () => {
       csvExport: false,
       headerClasses: "text-center",
       formatter: (rowcontent, row) => {
+        // console.log(row.cash_flow_type);
         return (
           <div className="row text-center">
             <div className={row.transaction_date === getToday() ? "col-12" : "d-none" }>
