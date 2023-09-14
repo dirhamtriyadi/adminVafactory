d5216ba8902d61cd4ef4e73de38416d5a08da886
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-09-14 13:01:47 +0700

    refactor: refactor sidebar menu

--- src/components/sidebar/menu.jsx
+++ src/components/sidebar/menu.jsx
@@ -6,17 +6,6 @@ const Menu = [
         role: "dashboard",
     },
     {
-        path: "#",
-        icon: "fa fa-hdd",
-        title: "Master",
-        isShow: true,
-        role: ["master-jenis-pembayaran", "master-kategori"],
-        children: [
-            { path: "/master-jenis-pembayaran", title: "Jenis Pembayaran", role: "master-jenis-pembayaran" },
-            { path: "/master-kategori", title: "Kategori", role: "master-kategori" },
-        ],
-    },
-    {
         path: "/data-customer",
         icon: "fas fa-users",
         title: "Data Customer",
@@ -31,21 +20,32 @@ const Menu = [
     {
         path: "#",
         icon: "fas fa-store",
-        title: "Penjualan",
+        title: "Penjualan Toko",
         role: ["data-barang", "penjualan", "lihat-penjualan"],
         children: [
-            { path: "/data-barang", title: "Data Barang", role: "data-barang" },
             { path: "/penjualan", title: "Transaksi Penjualan", role: "penjualan" },
             { path: "/lihat-penjualan", title: "Lihat Penjualan", role: "lihat-penjualan" },
+            { path: "/data-barang", title: "Data Barang", role: "data-barang" },
         ],
     },
-
     {
-        path: "/uang-kas",
-        icon: "fas fa-money-bill-alt",
-        title: "Uang Kas",
-        role: "uang-kas",
+        path: "#",
+        icon: "fas fa-archive",
+        title: "Orders",
+        role: ["orders", "order-transaction", "order-tracking"],
+        children: [
+            { path: "/orders", title: "Orders", role: "orders" },
+            { path: "/order-transaction", title: "Order Transaction", role: "order-transaction" },
+            { path: "/order-tracking", title: "Order Tracking", role: "order-tracking" },
+        ]
     },
+    // Uang Kas
+    // {
+    //     path: "/uang-kas",
+    //     icon: "fas fa-money-bill-alt",
+    //     title: "Uang Kas",
+    //     role: "uang-kas",
+    // },
     // {
     //     path: "/data-users",
     //     icon: "fas fa-users",
@@ -56,35 +56,52 @@ const Menu = [
     {
         path: "#",
         icon: "fas fa-paste",
-        title: "Laporan",
-        role: ["laporan-uang-kas"],
-        children: [{ path: "/laporan-uang-kas", title: "Laporan Uang Kas", role: "laporan-uang-kas" }],
+        title: "Uang Kas",
+        role: ["laporan-uang-kas", "uang-kas"],
+        children: [
+            { path: "/uang-kas", title: "Uang Kas", role: "uang-kas" },
+            { path: "/laporan-uang-kas", title: "Laporan Uang Kas", role: "laporan-uang-kas" }
+        ],
     },
 
     // Ade
+    // Tracking
+    // {
+    //     path: "/tracking",
+    //     icon: "fas fa-truck",
+    //     title: "Tracking",
+    //     role: "tracking",
+    // },
+    // Orders
+    // {
+    //     path: "/orders",
+    //     icon: "fas fa-archive",
+    //     title: "Orders",
+    //     role: "orders",
+    // },
+    // {
+    //     path: "/order-transaction",
+    //     icon: "fas fa-money-bill-alt",
+    //     title: "Order Transaction",
+    //     role: "order-transaction",
+    // },
+    // {
+    //     path: "/order-tracking",
+    //     icon: "fas fa-truck",
+    //     title: "Order Tracking",
+    //     role: "order-tracking",
+    // },
     {
-        path: "/tracking",
-        icon: "fas fa-truck",
-        title: "Tracking",
-        role: "tracking",
-    },
-    {
-        path: "/orders",
-        icon: "fas fa-archive",
-        title: "Orders",
-        role: "orders",
-    },
-    {
-        path: "/order-transaction",
-        icon: "fas fa-money-bill-alt",
-        title: "Order Transaction",
-        role: "order-transaction",
-    },
-    {
-        path: "/order-tracking",
-        icon: "fas fa-truck",
-        title: "Order Tracking",
-        role: "order-tracking",
+        path: "#",
+        icon: "fa fa-hdd",
+        title: "Master",
+        isShow: true,
+        role: ["master-jenis-pembayaran", "master-kategori", "tracking"],
+        children: [
+            { path: "/master-jenis-pembayaran", title: "Jenis Pembayaran", role: "master-jenis-pembayaran" },
+            { path: "/master-kategori", title: "Kategori", role: "master-kategori" },
+            { path: "/tracking", title: "Tracking", role: "tracking" },
+        ],
     },
     {
         path: "/data-users",
